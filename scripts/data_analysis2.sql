ab8ce3be143e2e53f5732f92622852534d01ed92
Nandeesha C G <147065442+NandiGowda003@users.noreply.github.com>
2026-02-06 20:01:53 +0530

    Update data_analysis2.sql

--- scripts/data_analysis2.sql
+++ scripts/data_analysis2.sql
@@ -109,3 +109,29 @@ FROM customer_spending
 )t
 GROUP BY cust_segment
 ORDER BY c DESC
+
+/*
+===============================================================================
+Part-to-Whole Analysis
+    - To compare performance or metrics across dimensions or time periods.
+    - To evaluate differences between categories.
+    - Useful for A/B testing or regional comparisons.
+===============================================================================
+*/
+
+-- Which categories contribute the most to overall sales?
+WITH category_sales AS (
+    SELECT
+        p.category,
+        SUM(f.sales_amount)  total_sales
+        FROM gold.fact_sales f
+    LEFT JOIN gold.dim_products p
+    ON f.product_key = p.product_key
+    GROUP BY p.category
+)
+SELECT
+    category,
+    total_sales,
+    CONCAT(ROUND(CAST(total_sales AS float)/ SUM(total_sales) OVER () * 100, 2),'%') AS percentage_of_total
+FROM category_sales
+ORDER BY total_sales DESC;
